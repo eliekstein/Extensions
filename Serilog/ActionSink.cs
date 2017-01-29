4284ecbf17160945c4220666e074ae211fb740a8
Eli Ekstein <eli@jafgifts.com>
2017-01-29 13:31:46 -0500

    added serilog action sink

---
+++ Serilog/ActionSink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Extensions.Serilog
+{
+    public class ActionSink : ILogEventSink
+    {
+        private Action<LogEvent> EventAction;
+        private Predicate<LogEventLevel> EventActionPredicate;
+
+        private ActionSink()
+        {
+
+        }
+        public ActionSink(Action<LogEvent> EventAction) : this()
+        {
+            this.EventAction = EventAction;
+        }
+        public ActionSink(Action<LogEvent> EventAction, Predicate<LogEventLevel> EventActionPredicate) : this(EventAction)
+        {
+            this.EventActionPredicate = EventActionPredicate;
+        }
+        public void Emit(LogEvent logEvent)
+        {
+            if (EventActionPredicate.Invoke(logEvent.Level))
+            {
+                EventAction(logEvent);
+            }
+        }
+    }
+}
