fdbb44fb6bd5b2f3eb2aad571fc0e242f94d6576
Eli Ekstein <eli@jafgifts.com>
2017-02-12 15:02:00 -0500

    Added eventhandler extension fire

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -89,5 +89,14 @@ namespace Extensions
             object sender,
             [CallerMemberName] String propertyName = "") =>
             notifier?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+
+        public static void Fire<TEventArgs>(this EventHandler eventobj,object sender,TEventArgs args) where TEventArgs : EventArgs
+        {
+            eventobj?.Invoke(sender, args);
+        }
+        public static void Fire(this EventHandler eventobj, object sender)
+        {
+            eventobj?.Invoke(sender,EventArgs.Empty);
+        }
     }
 }
