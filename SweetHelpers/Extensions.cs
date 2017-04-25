ac3a226e29b6cd9db1191c87d5196213823898f3
Eli Ekstein <eli@keestack.com>
2017-04-25 18:47:54 -0400

    Refactoring and file orgonization

--- SweetHelpers/Extensions.cs
+++ SweetHelpers/Extensions.cs
@@ -6,6 +6,45 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
+namespace SweetHelpers.Extensions
+{
+    public static class Extensions
+    {
+        public static DateTime AddDays(this DateTime date, double value, params DayOfWeek[] exceptWeekdays)
+        {
+            if (exceptWeekdays.Count() > 0)
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    if (exceptWeekdays.Any(d => d == date.AddDays((double)i).DayOfWeek))
+                        value += 1;
+                }
+            }
+            return date.AddDays(value);
+        }
+
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+        
+        public static string FormatWith(this string str, params object[] strs)
+        {
+            return string.Format(str, strs);
+        }
+
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source != null && toCheck != null && source.IndexOf(toCheck, comp) >= 0;
+        }
+        public static string Join(this string[] strings, string separator)
+        {
+            return string.Join(separator, strings);
+        }
+
+    }
+}
+
 namespace Extensions
 {
     public static class Extensions
@@ -108,44 +147,5 @@ namespace Extensions
         {
             eventobj?.Invoke(sender,EventArgs.Empty);
         }
-    }
-}
-
-namespace SweetHelpers.Extensions
-{
-    public static class Extensions
-    {
-        public static DateTime AddDays(this DateTime date, double value, params DayOfWeek[] exceptWeekdays)
-        {
-            if (exceptWeekdays.Count() > 0)
-            {
-                for (int i = 0; i < value; i++)
-                {
-                    if (exceptWeekdays.Any(d => d == date.AddDays((double)i).DayOfWeek))
-                        value += 1;
-                }
-            }
-            return date.AddDays(value);
-        }
-
-        public static bool IsNullOrEmpty(this string str)
-        {
-            return string.IsNullOrEmpty(str);
-        }
-        
-        public static string FormatWith(this string str, params object[] strs)
-        {
-            return string.Format(str, strs);
-        }
-
-        public static bool Contains(this string source, string toCheck, StringComparison comp)
-        {
-            return source != null && toCheck != null && source.IndexOf(toCheck, comp) >= 0;
-        }
-        public static string Join(this string[] strings, string separator)
-        {
-            return string.Join(separator, strings);
-        }
-
     }
 }
